eaa8c5487c4740728e7d6d77ec4fac196e43a0c7
Thiago Mateus <thiagomateus123@hotmail.com>
2023-03-16 11:14:52 -0300

    Implementando a lógica

--- App.js
+++ App.js
@@ -3,44 +3,100 @@ import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import ButtonCalc from './src/components/ButtonCalc';
 import Display from './src/components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: 'false',
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 
 export default function App() {
-  const [displayValue, setDisplayValue] = useState('1')
+  const [calcState, setCalcState] = useState(initialState)
 
-  addDigit = n =>{
-    setDisplayValue(n)
+  addDigit = n => {
+
+    const clearDisplay = calcState.displayValue === '0' || calcState.clearDisplay
+    
+    if (n === '.' && !clearDisplay && calcState.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : calcState.displayValue
+    const displayValue = currentValue + n
+
+    setCalcState(state => ({
+      ...state,
+      displayValue,
+      clearDisplay: false
+    }))
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...calcState.values]
+      values[calcState.current] = newValue
+      setCalcState(state=>({
+        ...state,
+        values: values
+      }))
+    }
   }
 
-  clearMemory = ()=>{
-    setDisplayValue('0')
+  clearMemory = () => {
+    setCalcState({...initialState})
   }
 
-  setOperation = operation =>{
+  setOperation = operation => {
+    if(calcState.current === 0){
+      setCalcState(state=>({
+        ...state,
+        operation: operation,
+        current:1,
+        clearDisplay: true
+      }))
+    } else{
+      const equals = operation ==='='
+      const values = [...calcState.values]
+      try{
+        values[0] = eval(`${values[0]} ${calcState.operation} ${values[1]}`)
+      } catch (e){
+        values[0] = calcState.values[0]
+      }
+
+      values[1] = 0
+      setCalcState(state=>({
+        ...state,
+        displayValue: `${values[0]}`,
+        operation: equals? null : operation,
+        clearDisplay: !equals,
+        values
+      }))
+    }
 
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <Display value={displayValue} />
+      <Display value={calcState.displayValue} />
       <View style={styles.buttons}>
-        <ButtonCalc label='AC' triple onClick={clearMemory}/>
-        <ButtonCalc label='/' operation onClick={setOperation}/>
-        <ButtonCalc label='7' onClick={addDigit}/>
-        <ButtonCalc label='8' onClick={addDigit}/>
-        <ButtonCalc label='9' onClick={addDigit}/>
-        <ButtonCalc label='*' operation onClick={setOperation}/>
-        <ButtonCalc label='4' onClick={addDigit}/>
-        <ButtonCalc label='5' onClick={addDigit}/>
-        <ButtonCalc label='6' onClick={addDigit}/>
-        <ButtonCalc label='-' operation onClick={setOperation}/>
-        <ButtonCalc label='1' onClick={addDigit}/>
-        <ButtonCalc label='2' onClick={addDigit}/>
-        <ButtonCalc label='3' onClick={addDigit}/>
-        <ButtonCalc label='+' operation onClick={setOperation}/>
-        <ButtonCalc label='0' double onClick={addDigit}/>
-        <ButtonCalc label='.' onClick={addDigit}/>
-        <ButtonCalc label='=' operation onClick={setOperation}/>
+        <ButtonCalc label='AC' triple onClick={clearMemory} />
+        <ButtonCalc label='/' operation onClick={setOperation} />
+        <ButtonCalc label='7' onClick={addDigit} />
+        <ButtonCalc label='8' onClick={addDigit} />
+        <ButtonCalc label='9' onClick={addDigit} />
+        <ButtonCalc label='*' operation onClick={setOperation} />
+        <ButtonCalc label='4' onClick={addDigit} />
+        <ButtonCalc label='5' onClick={addDigit} />
+        <ButtonCalc label='6' onClick={addDigit} />
+        <ButtonCalc label='-' operation onClick={setOperation} />
+        <ButtonCalc label='1' onClick={addDigit} />
+        <ButtonCalc label='2' onClick={addDigit} />
+        <ButtonCalc label='3' onClick={addDigit} />
+        <ButtonCalc label='+' operation onClick={setOperation} />
+        <ButtonCalc label='0' double onClick={addDigit} />
+        <ButtonCalc label='.' onClick={addDigit} />
+        <ButtonCalc label='=' operation onClick={setOperation} />
       </View>
     </SafeAreaView>
   );
