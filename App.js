86bc29c1c060dd42b5bf6909af602a33ee96fd10
Thiago Mateus <thiagomateus123@hotmail.com>
2023-03-16 09:50:29 -0300

    configurando os botões

--- App.js
+++ App.js
@@ -5,30 +5,42 @@ import Display from './src/components/Display';
 
 
 export default function App() {
-  const [displayValue,setDisplayValue] = useState('0')
+  const [displayValue, setDisplayValue] = useState('1')
+
+  addDigit = n =>{
+    setDisplayValue(n)
+  }
+
+  clearMemory = ()=>{
+    setDisplayValue('0')
+  }
+
+  setOperation = operation =>{
+
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <Display value={displayValue}/>
+      <Display value={displayValue} />
       <View style={styles.buttons}>
-        <ButtonCalc label='AC'/>
-        <ButtonCalc label='/'/>
-        <ButtonCalc label='7'/>
-        <ButtonCalc label='8'/>
-        <ButtonCalc label='9'/>
-        <ButtonCalc label='*'/>
-        <ButtonCalc label='4'/>
-        <ButtonCalc label='5'/>
-        <ButtonCalc label='6'/>
-        <ButtonCalc label='-'/>
-        <ButtonCalc label='1'/>
-        <ButtonCalc label='2'/>
-        <ButtonCalc label='3'/>
-        <ButtonCalc label='+'/>
-        <ButtonCalc label='0'/>
-        <ButtonCalc label='.'/>
-        <ButtonCalc label='='/>
+        <ButtonCalc label='AC' triple onClick={clearMemory}/>
+        <ButtonCalc label='/' operation onClick={setOperation}/>
+        <ButtonCalc label='7' onClick={addDigit}/>
+        <ButtonCalc label='8' onClick={addDigit}/>
+        <ButtonCalc label='9' onClick={addDigit}/>
+        <ButtonCalc label='*' operation onClick={setOperation}/>
+        <ButtonCalc label='4' onClick={addDigit}/>
+        <ButtonCalc label='5' onClick={addDigit}/>
+        <ButtonCalc label='6' onClick={addDigit}/>
+        <ButtonCalc label='-' operation onClick={setOperation}/>
+        <ButtonCalc label='1' onClick={addDigit}/>
+        <ButtonCalc label='2' onClick={addDigit}/>
+        <ButtonCalc label='3' onClick={addDigit}/>
+        <ButtonCalc label='+' operation onClick={setOperation}/>
+        <ButtonCalc label='0' double onClick={addDigit}/>
+        <ButtonCalc label='.' onClick={addDigit}/>
+        <ButtonCalc label='=' operation onClick={setOperation}/>
       </View>
     </SafeAreaView>
   );
@@ -38,8 +50,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  buttons:{
-    flexDirection:'row',
-    flexWrap:'wrap'
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   }
 });
--- src/components/ButtonCalc.js
+++ src/components/ButtonCalc.js
@@ -1,10 +1,22 @@
 import { StyleSheet, Dimensions, TouchableHighlight, Text } from 'react-native'
 import React from 'react'
 
-const ButtonCalc = ({onClick,label}) => {
+const ButtonCalc = ({ onClick, label, double, triple, operation }) => {
+
+    const stylesButton = [style.button]
+
+    if (double)
+        stylesButton.push(style.buttonDouble)
+
+    if (triple)
+        stylesButton.push(style.buttonTriple)
+
+    if (operation)
+        stylesButton.push(style.operationButton)
+
     return (
-        <TouchableHighlight onPress={onClick}>
-            <Text style={style.button}>{label}</Text>
+        <TouchableHighlight onPress={() => onClick(label)}>
+            <Text style={stylesButton}>{label}</Text>
         </TouchableHighlight>
     )
 }
@@ -14,11 +26,21 @@ const style = StyleSheet.create({
         fontSize: 40,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
-        padding:20,
-        backgroundColor:'#f0f0f0',
+        padding: 20,
+        backgroundColor: '#f0f0f0',
         textAlign: 'center',
-        borderWidth:1,
-        borderColor:'#888'
+        borderWidth: 1,
+        borderColor: '#888'
+    },
+    operationButton: {
+        color: 'white',
+        backgroundColor: 'orange'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3
     }
 })
 
